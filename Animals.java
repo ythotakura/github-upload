054e253d931a8ca48742d75f1f2d508580a03fb8
Yashili Thotakura <yashili.thotakura@gmail.com>
2021-11-29 23:08:48 -0500

    initializing repository        /updated author

---
+++ Animals.java
@@ -0,0 +1,7 @@
+public class Animals 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
---
+++ HideAndSeek.java
@@ -0,0 +1,11 @@
+public class HideAndSeek 
+{
+    public static void main(String[] args) 
+    {
+      System.out.println("Let's play hide and seek.");
+      System.out.println("Three...");
+      System.out.println("Two...");
+      System.out.println("One...");
+      System.out.println("Ready or not, here I come!");
+    }
+}
