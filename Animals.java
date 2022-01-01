7365489db3a8bb35c22ef1d359af2436a80b1089
Yashili Thotakura <yashili.thotakura@gmail.com>
2022-01-01 15:06:04 -0500

    changed Amnimals file

--- Animals.java
+++ Animals.java
@@ -1,7 +1,51 @@
 public class Animals 
 {
-    public static void main(String[] args) 
+    
+    String text;
+    String yash;
+    int val;
+
+    public Animals() {  }
+
+    public Animals(String text) {
+        this.text = text;
+    }
+
+    public Animals(String text, String yash, int val) {
+        this.yash = yash;
+    }
+
+    public Animals(int val)
     {
-        
+        this.val = val;
+    }
+
+    public Animals(String text, int val)
+    {
+        this.text = text;
+        this.val = val;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setVal(int val) {
+
+        this.val = val;
+
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public String toString() {
+        return text + " - " + getVal();
     }
 }
