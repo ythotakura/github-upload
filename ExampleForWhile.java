ac22f0d99eb06631c2fb3f480aa1162980b3e831
Yashili Thotakura <yashili.thotakura@gmail.com>
2022-01-01 15:56:43 -0500

    added Scanner

--- ExampleForWhile.java
+++ ExampleForWhile.java
@@ -1,5 +1,16 @@
-package io.github.jiangdequan;
-
-public class ExampleForWhile {
-    
+import java.util.Scanner;
+public class ExampleForWhile 
+{
+    public static void main(String [] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        
+    }
 }
+
+/*
+ * ask user how many dice rolls
+ * check if integer for positive and greater than 0
+ * get a random number for each dice roll
+ * print number
+ */
