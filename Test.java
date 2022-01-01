059821d94fac7dc62f8e229b1023fda129f7ccb5
Yashili Thotakura <yashili.thotakura@gmail.com>
2022-01-01 15:53:55 -0500

    formatted

--- Test.java
+++ Test.java
@@ -1,39 +1,19 @@
 public class Test 
 {
+    public static void main(String[] args) 
+    {
+        Animals ani = new Animals();
+        ani.setText("Monkey");
+        ani.setVal(10);
+        Animals ani1 = new Animals("Dinosaur");
+        System.out.println(ani1.toString());
 
-      public static void main(String[] args) {
-        // int num = 1327;
-        // while(num % 3 == 0 && (num % 5 != 0) && num <= 4542)
-        // {
-        //     System.out.println(num);
-        //         //if(num % 3 == 0 && num % 5 != 0){
-        //     //count++;
-        //     //}
-        //     num++;
-        // }
+        Animals ani2 = new Animals(15);
+        ani2 = new Animals("test");
+        ani2.setVal(10);
+        System.out.println(ani2.toString());
 
-          Animals ani = new Animals();
-
-          ani.setText("Monkey");
-          ani.setVal(10);
-         
-          //System.out.println(ani.toString());
-
-          Animals ani1 = new Animals("Dinosaur");
-          System.out.println(ani1.toString());
-
-          Animals ani2 = new Animals(15);
-          ani2 = new Animals("test");
-          ani2.setVal(10);
-          //ani2.setText("Dog");
-          System.out.println(ani2.toString());
-
-         Animals ani3  = new Animals("hi", 19);
-         System.out.println(ani3.toString());
-          
-
-          // call a constructor that will take text
-
-
-      }  
+        Animals ani3  = new Animals("hi", 19);
+        System.out.println(ani3.toString());
+    }  
 }
