9efcf540f359f2e0bd99a3ab818848271cd9fc81
Yashili Thotakura <yashili.thotakura@gmail.com>
2022-01-04 10:51:56 -0500

    Small program that prints stuff.

--- Creator.java
+++ Creator.java
@@ -2,6 +2,7 @@ public class Creator
 {
     public static void main(String[] args) 
     {
+        // Declares two variables and prints them.
         String name = "James Gosling";
         int yearCreated = 1995;
         System.out.println(name + ", " + yearCreated);
