668033dade464c065a8915e0dce58305eec9cf27
Yashili Thotakura <yashili.thotakura@gmail.com>
2022-01-04 10:49:17 -0500

    Gets random numbers from a dice.

--- ExampleForWhile.java
+++ ExampleForWhile.java
@@ -1,15 +1,24 @@
+// Import statements needed for this class.
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.math.*;
+
 public class ExampleForWhile 
 {
     public static void main(String [] args) 
     {
+        // Variables needed for this class.
         int math = 0;
         int num = 0;
+
+        // ArrayList that holds the dice values.
         ArrayList <Integer> diceRoll = new ArrayList <Integer> ();
+
+        // Asks the user how many times they want to roll the dice.
         Scanner input = new Scanner(System.in);
         System.out.println("How many times do you want to roll the dice?");
+
+        // This while loop checks if the value given is an integer and positive.
+        // Will ask the user to give another value if it does not meet the conditions.
         while(num <= 0)
         {
             if(input.hasNextInt())
@@ -28,11 +37,16 @@ public class ExampleForWhile
                 input.next();
             }
         }
+
+        // This for loop gets the random dice values and puts them into the ArrayList.
         for(int i = 0; i < num; i++)
         {
             math = (int)((Math.random() * 6) + 1);
             diceRoll.add(math);
         }
+
+        // Prints the values.
         System.out.println("Your rolls: " + diceRoll);
+        input.close();
     }
 }
