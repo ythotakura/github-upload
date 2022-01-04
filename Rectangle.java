4789643ba38d0a2be9b7c7676b0ef7ff0c24d3f3
Yashili Thotakura <yashili.thotakura@gmail.com>
2022-01-04 10:39:44 -0500

    Calculates the area and perimeter of a rectangle.

--- Rectangle.java
+++ Rectangle.java
@@ -1,19 +1,24 @@
+// The class Rectangle implements the interface Shape.
 public class Rectangle implements Shape
 {
+    // Variables used in this class.
     private int width;
     private int height;
     
+    // Constructor that sets the values for the width and the height.
     public Rectangle(int w, int h)
     {
         width = w;
         height = h;
     }
 
+    // Calculates the area of a rectangle.
     public double getArea()
     {
         return (width * height);
     }
 
+    // Calculates the perimeter of a rectangle.
     public double getPerimeter()
     {
         return((width * 2) + (height * 2));
