3912d72f64e0a83d493a3dd6304d51f8f7bdb70f
Yashili Thotakura <yashili.thotakura@gmail.com>
2022-01-04 10:38:24 -0500

    Calculates the area and perimeter of a circle.

--- Circle.java
+++ Circle.java
@@ -1,16 +1,22 @@
+// The class Circle implements the interface Shape.
 public class Circle implements Shape
 {
+    // Variable used in this class.
     private int radius;
+
+    // Constructor that sets the value for the radius.
     public Circle(int r)
     {
         radius  = r;
     }
 
+    // Calculates the area of a circle.
     public double getArea()
     {
         return (3.14 * radius * radius);
     }
 
+    // Calculates the perimeter of a circle.
     public double getPerimeter()
     {
         return(2 * 3.14 * radius);
